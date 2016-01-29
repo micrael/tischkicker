2a8b03f5fafb166897501280b22993e0d238ea61
Julian Doll <Julian.Doll@EXXETA.com>
2016-01-29 08:54:10 +0100

    Activated live mongodb.

--- tischkicker-backend/index.js
+++ tischkicker-backend/index.js
@@ -1,8 +1,8 @@
 var mongo = require('mongodb');
 
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://172.16.64.135:27017/tischkicker');
-mongoose.connect('mongodb://172.16.64.58:27017/tischkicker');
+mongoose.connect('mongodb://172.16.64.135:27017/tischkicker');
+//mongoose.connect('mongodb://172.16.64.58:27017/tischkicker');
 
 var Goal = mongoose.model('Goal', { team: String, speed: String });
 
